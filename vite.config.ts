155d9d8d8961ec7340a4611a0f3355a46b0b6b8f
shahdyasser7 <shahdyaser4@icloud.com>
2026-06-24 00:55:49 +0300

    chore: increase vite chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    chunkSizeWarningLimit: 800,
+    chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
         manualChunks: undefined,
